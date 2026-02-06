c09f048ee380205f35467ce77000de346f64a176
Sriram R <sripraveen0372@gmail.com>
2026-02-06 16:18:58 +0530

    Use env-based API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,18 +1,15 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
-// 🔥 ATTACH TOKEN TO EVERY REQUEST
-api.interceptors.request.use((config) => {
+api.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
-
   if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+    req.headers.Authorization = `Bearer ${token}`;
   }
-
-  return config;
+  return req;
 });
 
 export default api;
