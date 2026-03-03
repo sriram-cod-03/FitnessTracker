c31529b595af24052efa7c72f54896cc79a38317
Sriram R <sripraveen0372@gmail.com>
2026-03-03 07:40:49 +0530

    Fixed MongoDB Atlas connection

--- backend/server.js
+++ backend/server.js
@@ -34,8 +34,8 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(PORT, () =>
-      console.log(`Server running on port ${PORT}`)
-    );
+    app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
   })
   .catch((err) => console.log(err));
