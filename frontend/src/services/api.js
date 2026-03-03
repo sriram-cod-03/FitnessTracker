164a0ca9fb8240bb86fa4746c60857ca847f9ecd
Sriram R <sripraveen0372@gmail.com>
2026-03-03 17:54:33 +0530

    Backend API URL change from frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://fitnesstracker-cbhi.onrender.com/api",
+  baseURL: "https://fitnesstracker-0a0d.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
