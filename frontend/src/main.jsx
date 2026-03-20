32beec2e33e16ee5a2cdfba80e274685c9103264
Sriram R <sripraveen0372@gmail.com>
2026-03-20 20:56:15 +0530

    update the bootstrap icons

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import "bootstrap/dist/css/bootstrap.min.css";
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import "./styles/toast.css";
 import "./styles/navbar.css";
 import { Toaster } from "react-hot-toast";
